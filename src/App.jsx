f742f657bb3728c3533913477e18703d7fd3bd6d
Andrei Moshchenko <99655206+AndrewMosh@users.noreply.github.com>
2023-08-02 14:31:20 +0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className=" w-full relative lg:bg-gray-100 h-screen border-2 border-blue-900 p-5 m-0 flex flex-col  gap-20 items-center justify-center">
+      <h1 className="absolute hover:scale-110 cursor-pointer text-3xl rounded-full  p-20 bg-green-200 font-bold text-blue-900 text-center  ">
+        Hello Vite + React
+      </h1>
+      <p className="bg-red-200 p-20 rounded-full hover:scale-110 hover:text-blue-900 cursor-pointer text-5xl text-grey-darkest">
+        Vite + React
+      </p>
+      <div className="text-4xl text-purple-500">уиииии </div>
+    </div>
+  );
+}
+
+export default App;
